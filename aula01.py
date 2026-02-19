7729238264323860c840e6e7c3df34ad988597ce
Melissa Vitória <melissa.silva01097480@sesisenaipr.org.br>
2026-02-19 10:04:36 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteira
+peso = 60 # variável float
+nome = "Melissa Vitória Fernandes da Silva"
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
