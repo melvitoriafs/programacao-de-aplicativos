d7a7c63c15b9b8a22942a0801785f6c17f2cea25
mel <melissa.silva01097480@sesisenaipr.org.br>
2026-05-13 12:32:36 -0300

    13/05

---
+++ 2trimestre/destino.py
@@ -0,0 +1,64 @@
+def criar_arquivo():
+    open('viagens.txt', 'w').close()
+criar_arquivo()
+
+def criar():
+    lugar = input("Sugere algum lugar: ")
+    with open('viagens.txt', 'a')as f:
+        f.write(lugar + '\n')
+    print("Lugares cadastrados ")
+
+def ler():
+    with open('viagens.txt', 'r')as f:
+        destino = f.readlines()
+
+        i = 0 
+        for destino in destinos:
+            print(f"{i} - {destino.strip()}")
+            i += 1
+
+def atualizar():
+    ler()
+    idx = int(input("Digite o id do lugar que deseja alterar: "))
+    novo_lugar = input("Novo lugar: ")
+
+    with open('viagens.txt', 'r')as f:
+        linhas = f.readlines()
+
+    linhas[idx] = novo_lugar + '\n'
+
+    with open('viagens.txt', 'w')as f:
+        f.writelines(linhas)
+    print("Lugares atualizados ")
+
+def deletar():
+    ler()
+    idx = int(input("Digite o id do lugar que deseja excluir: "))
+    with open('viagens.txt', 'r')as f:
+        linhas = f.readlines()
+    del linhas[idx]
+    with open('viagens.txt', 'w')as f:
+        f.writelines(linhas)
+    print("Lugar removido ")
+
+while True: 
+    print("\n1- Adicionar destino")
+    print("\n2- Listar sugestões")
+    print("\n3- Editar sugestão")
+    print("\n4- Remover sugestão")
+    print("\n5- Sair   ")
+    opcao = int(input("\nEscolha: "))
+
+    if opcao == '1': adicionar()
+    elif opcao == '2': ler()
+    elif opcao == '3': atualizar()
+    elif opcao == '4': deletar()
+    elif opcao == '5': 
+        print("Programa encerrado ")
+        break
+
+
+
+
+
+
