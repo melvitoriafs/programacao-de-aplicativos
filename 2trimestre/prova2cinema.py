6a35a5a7f218edb9f1b1014dff7be374d03d2282
mel <melissa.silva01097480@sesisenaipr.org.br>
2026-08-07 10:09:10 -0300

    07/08

--- 2trimestre/prova2cinema.py
+++ 2trimestre/prova2cinema.py
@@ -8,17 +8,17 @@ def cadastrar_cinemas():
                 CREATE TABLE  IF NOT EXISTS cinemas (
                 id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL,
-                cidade TEXT NOT NULL
+                shopping TEXT NOT NULL
             )
             ''')
     
     try:
         nome_cinema = input("Digite o nome do cinema: ")
-        nome_cidade = input("Digite o nome da cidade: ")
+        shopping = input("Digite o nome do shopping: ")
 
-        comando_inserir = (F'''
-                            (INSERT INTO cinemas (nome, cidade))
-                            VALUES ('{nome_cinema}', '{nome_cidade}')''')
+        comando_inserir = (f'''
+                            (INSERT INTO cinemas (nome, shopping))
+                            VALUES ('{nome_cinema}', '{shopping}')''')
     except ValueError as erro:
         print("Digite apenas o nome!")
 
@@ -32,4 +32,29 @@ def cadastrar_cinemas():
         conexao.close()
 
 def cadastrar_salas():
-    
+    conexao = sqlite3.connect('sistema_cinema.db')
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS cinemas (
+                id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+                numero_sala INTEGER,
+                capacidade INTEGER UNIQUE NOT NULL,
+                id_cinema INTEGER,
+                FOREIGN KEY (id_cinemas) REFERENCES cinemas(id)
+                )
+                ''')
+
+    try:
+        numero_sala = int(input("Digite o número da sala:"))
+        capacidade = int(input("Digite a capacidade de pessoas: "))
+        id_cinema = int(input("Digite o id do cinema: "))
+
+    comando_inserir = (f'''
+                        (INSERT TO cinemas(numero_sala, capacidade, id_cinema))
+                        VALUES ('{nome_cinema}', '{capacidade}',{id_cinema})''')
+
+luis é lindo e todos sabem, porem ninguem confia em um ex mentiroso!!!!!!! sou de familia e nao faço nada 
+de interessante na minha vida, porem eu tenho muito a oferecer
+sou desse jeito rustico mas sou apaixonado
+espero que todos que leiam essa mensagem, arrumem uma namorada para mim!!!!!!!!!!!
